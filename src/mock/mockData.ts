634d36378859581d4a54f4127275da2a7649d58d
iTwsT <nowakhtha@gmail.com>
2025-08-24 22:15:17 +0300

    project structure and initial code population

---
+++ src/mock/mockData.ts
@@ -0,0 +1,17 @@
+const MOCK_MENU = [...];
+const MOCK_CUSTOMERS = [...];
+const MOCK_ORDERS = [...];
+const MOCK_BAYS = [...];
+const MOCK_ANALYTICS_DAILY = [...];
+const MOCK_TOP_ITEMS = [...];
+const STATUS_COLORS = [...];
+
+export {
+  MOCK_MENU,
+  MOCK_CUSTOMERS,
+  MOCK_ORDERS,
+  MOCK_BAYS,
+  MOCK_ANALYTICS_DAILY,
+  MOCK_TOP_ITEMS,
+  STATUS_COLORS,
+};
